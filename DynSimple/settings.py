11dd008f1d1dcc3befcc4953e330266e09c14f2a
zuck007 <pradeepchoudhary009@gmail.com>
2017-08-28 23:21:20 +0530

    need little more work

--- DynSimple/settings.py
+++ DynSimple/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'dyform.apps.DyformConfig',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -121,5 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
